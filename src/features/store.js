18693488f666e08c0a75f116b6171d2de6e7ba2c
kothasreekanthreddy <kothasreekanth2021@gmail.com>
2022-03-08 14:15:20 +0530

    ReactJS Assignment

---
+++ src/features/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import albumsReducer from "./albums/albumSlice";
+
+export const store = configureStore({
+    reducer: {
+        albums:albumsReducer
+    }
+});
